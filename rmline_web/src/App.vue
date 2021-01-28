148b666b092c2de141d66b8d26dae1edf4088935
mtx <mtcode@163.com>
2021-01-29 00:14:36 +0800

    1

--- rmline_web/src/App.vue
+++ rmline_web/src/App.vue
@@ -4,11 +4,16 @@
     <my-add></my-add>
 
 
-    <p>------------</p>
+    <p ref="p">------------</p>
 
 
     <my-sub></my-sub>
+    <div :style="styleObj1" style="height: 2000px">
+
+    </div>
+    <h1 ref="p2" :class="classObj">hello world</h1>
   </div>
+
 
 </template>
 
@@ -16,10 +21,30 @@
   import Addition from "@/components/Addition";
   import Subtraction from "@/components/Subtraction";
   export default {
-
+    mounted() {
+      window.addEventListener("scroll",this.getScroll)
+    },
+    destroyed() {
+      window.removeEventListener("scroll",this.getScroll)
+    },
+    methods:{
+      getScroll(){
+        let scrollTop = window.pageYOffset|| document.documentElement.scrollTop || document.body.scrollTop;
+        if (scrollTop>this.$refs.p.offsetHeight){
+          this.styleObj1 = {'background-color':'green'}
+        }
+        if (scrollTop>this.$refs.p2.offsetTop-window.innerHeight	){
+          this.classObj = 'animated bounceInDown'
+        }
+        console.log(this.$refs.p2.offsetTop-window.innerHeight	+" "+scrollTop)
+      }
+    },
     data(){
       return{
-
+        styleObj1:{
+          'background-color':'black',
+        },
+        classObj:'',
       };
     },
     components:{
--- rmline_web/src/main.js
+++ rmline_web/src/main.js
@@ -11,7 +11,6 @@ Vue.config.productionTip = false
 
 
 
-
 new Vue({
   router,
   store,
