0296718628e4a84b5392ea55c4b4feae45e62ded
mtx <mtcode@163.com>
2021-01-30 23:40:50 +0800

    1

--- rmline_web/src/App.vue
+++ rmline_web/src/App.vue
@@ -1,19 +1,55 @@
 <template>
   <div>
-    <Bar_top></Bar_top>
+    <Bar_top v-if="showTopBar" :class="TopBarClass"
+             style="animation-duration: 0.5s;position: fixed;top: 0;left: 0;width: 100%"></Bar_top>
+    <div style="height: 3000px">
+
+    </div>
   </div>
 </template>
 
 <script>
-  import Bar_top from "@/components/Bar_top";
-  export default {
-    components: {Bar_top}
-  };
+import Bar_top from "@/components/Bar_top";
+
+export default {
+  mounted() {
+    window.addEventListener("scroll", this.getScroll)
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.getScroll)
+  },
+  methods: {
+    getScroll() {
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+      this.TopBarClass = ''
+      if (scrollTop > 0) {
+        this.TopBarClass = 'animated fadeInDown'
+        this.showTopBar = true;
+      } else if (scrollTop === 0) {
+        this.TopBarClass = 'animated fadeOutUp'
+        setTimeout(function () {
+          this.showTopBar = false;
+        }, 500)
+      }
+    }
+  },
+  data() {
+    return {
+      showTopBar: true,
+      TopBarClass: ''
+    }
+  },
+
+  components: {Bar_top}
+};
 </script>
 
 <style>
-/*body{*/
-/*  margin:0;*/
-/*  padding:0;*/
-/*}*/
+@import "font/font.css";
+@import "http://localhost:81/css/Bar_top.css";
+
+body {
+  margin: 0;
+  padding: 0;
+}
 </style>
--- rmline_web/src/components/Bar_top.vue
+++ rmline_web/src/components/Bar_top.vue
@@ -1,24 +1,84 @@
 <template>
   <div>
-    <el-row :gutter="30" style="height: 20px" >
-      <el-col :span="6"><div class="grid-content bg-purple" style="background-color: black;height: 20px" ></div></el-col>
-      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    <el-row id="top_bar" :gutter="30">
+      <el-col :span="8">
+        <el-row>
+          <el-col :span="13">
+            <div style="height: 1px"></div>
+          </el-col>
+          <el-col :span="2">
+            <div class="search"></div>
+          </el-col>
+          <el-col :span="4"><p class="search_p">Search</p></el-col>
+          <el-col :span="5">
+            <div style="height: 1px"></div>
+          </el-col>
+        </el-row>
+      </el-col>
+      <el-col :span="8">
+        <el-row>
+          <el-col :span="10">
+            <div style="height: 1px"></div>
+          </el-col>
+          <el-col :span="4">
+            <div class="logo"></div>
+          </el-col>
+          <el-col :span="8">
+            <div style="height: 1px"></div>
+          </el-col>
+        </el-row>
+      </el-col>
+      <el-col :span="8">
+        <el-row>
+          <el-col :span="7">
+            <div style="height: 1px"></div>
+          </el-col>
+          <el-col :span="5" :class="menu_class" @mouseenter.native="menu_over()" @mouseleave.native="menu_leave()">
+            <el-row>
+              <el-col :span="10"><p class="search_p">MENU</p></el-col>
+              <el-col  :span="7">
+                <div class="MENU">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+              </el-col>
+            </el-row>
+          </el-col>
+          <el-col :span="12">
+            <div style="height: 1px"></div>
+          </el-col>
+        </el-row>
+
+      </el-col>
     </el-row>
-    <el-button type="primary">主要按钮</el-button>
-    <el-button type="warning">警告按钮</el-button>
   </div>
 </template>
 
 <script>
-// import "@/static/css/Bar_top.css"
 export default {
+
   name: "Bar_top",
+  methods:{
+    menu_over(){
+      this.menu_class='animated rubberBand'
+    },
+    menu_leave(){
+      this.menu_class=''
+    },
+
+  },
+  data(){
+    return{
+      menu_class:''
+
+    }
+  }
+
 }
 </script>
 
 <style scoped>
-  @import "http://localhost:81/css/Bar_top.css";
+
 
 </style>
