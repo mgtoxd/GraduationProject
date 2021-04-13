60e7abacd3620cd9273696482844dc80a1004159
mtx <2359971480@qq.com>
2021-04-13 08:01:32 +0800

    wda

--- backyard/resources/src/main/java/pers/mtx/resources/controller/ImgController.java
+++ backyard/resources/src/main/java/pers/mtx/resources/controller/ImgController.java
@@ -1,30 +1,96 @@
 package pers.mtx.resources.controller;
 
-import io.minio.GetObjectArgs;
-import io.minio.MinioClient;
+import io.minio.*;
+import jdk.internal.util.xml.impl.Input;
+import lombok.SneakyThrows;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import sun.awt.image.JPEGImageDecoder;
+import sun.awt.image.PNGImageDecoder;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Calendar;
+import java.util.UUID;
 
 @Controller
 public class ImgController {
     @Autowired
     MinioClient client;
-    @GetMapping("/img/{bk}/{name}")
-    public void getPic(@PathVariable("bk") String bucket, @PathVariable("name") String name, HttpServletRequest request, HttpServletResponse response){
-        try (InputStream inputStream = client.getObject(GetObjectArgs.builder().bucket(bucket).object(name).build());
-             OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());) {
+    @SneakyThrows
+    @PostMapping(value = "/fileUpload")
+    public String fileUpload(@RequestParam(value = "file") MultipartFile file) {
+        if (file.isEmpty()) {
+            System.out.println("文件为空空");
+        }
+        //使用默认时区和语言环境获得一个日历。
+        Calendar rightNow    =    Calendar.getInstance();
+        Integer year = rightNow.get(Calendar.YEAR);
+        Integer month = rightNow.get(Calendar.MONTH)+1; //第一个月从0开始，所以得到月份＋1
+        Integer day = rightNow.get(Calendar.DAY_OF_MONTH);
+        String bucket = year+"-"+month;
+        boolean exists = client.bucketExists(BucketExistsArgs.builder().bucket(bucket).build());
+        if (!exists){
+            client.makeBucket(MakeBucketArgs.builder().bucket(bucket).build());
+        }
+        InputStream go = file.getInputStream();
+        String fileName = UUID.randomUUID()+".png";
+        client.putObject(PutObjectArgs.builder().bucket(bucket).stream(go,go.available(),-1).object(fileName).contentType("image/png").build());
+        return fileName;
+
+    }
+
+
+    @RequestMapping("/img/{bk}/{name}")
+    public void getPic(@RequestParam("w") int width,@RequestParam("h") int height,@PathVariable("bk") String bucket, @PathVariable("name") String name, HttpServletRequest request, HttpServletResponse response){
+        System.out.println(bucket);
+        System.out.println(name);
+        try  {
+            InputStream inputStream = client.getObject(GetObjectArgs.builder().bucket(bucket).object(name).build());
+            OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
+//            inputStream = zoomBySize(width,height,inputStream);
+            assert inputStream != null;
             IOUtils.copy(inputStream, outputStream);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 指定长和宽对图片进行缩放
+     * @param width 长
+     * @param height 宽
+     * @throws IOException
+     */
+    public static InputStream zoomBySize(int width, int height, InputStream img) throws IOException {
+        //与按比例缩放的不同只在于,不需要获取新的长和宽,其余相同.
+        BufferedImage image = ImageIO.read(img);
+        Image _img = image.getScaledInstance(width, height, Image.SCALE_DEFAULT);
+        BufferedImage image1 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = image1.createGraphics();
+        graphics.drawImage(_img, 0, 0, null);
+        graphics.dispose();
+//        OutputStream out = new FileOutputStream(destFile);
+//        ImageIO.write(imageC, "png", out);
+
+//        out.close();
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        try {
+            File imgZoomFile = new File("D:\\bb3.png");
+            ImageIO.write(image1, "PNG", imgZoomFile);
+            return new ByteArrayInputStream(os.toByteArray());
+        } catch (IOException e) {
+            System.out.println("go");
+            return null;
         }
     }
 }
--- backyard/resources/src/test/java/pers/mtx/resources/ResourcesApplicationTests.java
+++ backyard/resources/src/test/java/pers/mtx/resources/ResourcesApplicationTests.java
@@ -1,63 +1,61 @@
-//package pers.mtx.resources;
-//
-//import io.minio.*;
-//import io.minio.errors.*;
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import java.io.*;
-//import java.nio.charset.StandardCharsets;
-//import java.security.InvalidKeyException;
-//import java.security.NoSuchAlgorithmException;
-//import java.text.DateFormat;
-//import java.text.SimpleDateFormat;
-//import java.time.LocalDate;
-//import java.time.LocalDateTime;
-//import java.time.LocalTime;
-//import java.util.Calendar;
-//import java.util.Date;
-//
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = ResourcesApplication.class)
-//class ResourcesApplicationTests {
-//
-//    @Autowired
-//    MinioClient client;
-//
-//    @Test
-//    void contextLoads() throws IOException, InvalidKeyException, InvalidResponseException, InsufficientDataException, NoSuchAlgorithmException, ServerException, InternalException, XmlParserException, ErrorResponseException {
-//        //使用默认时区和语言环境获得一个日历。
-//        Calendar rightNow    =    Calendar.getInstance();
-//        /*用Calendar的get(int field)方法返回给定日历字段的值。
-//        HOUR 用于 12 小时制时钟 (0 - 11)，HOUR_OF_DAY 用于 24 小时制时钟。*/
-//        Integer year = rightNow.get(Calendar.YEAR);
-//        Integer month = rightNow.get(Calendar.MONTH)+1; //第一个月从0开始，所以得到月份＋1
-//        Integer day = rightNow.get(Calendar.DAY_OF_MONTH);
-//        String bucket = year+"-"+month;
-//        boolean exists = client.bucketExists(BucketExistsArgs.builder().bucket(bucket).build());
-//        if (!exists){
-//            client.makeBucket(MakeBucketArgs.builder().bucket(bucket).build());
-//
-//        }
-//        File img = new File("D:\\demo.jpg");
-//        InputStream go = new FileInputStream(img);
-//        InputStream stream = new ByteArrayInputStream("wdawdawdawdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasssssssssssssssssssssssssssssddddddddddddddddddddddddddddddd".getBytes(StandardCharsets.UTF_8));
-//        client.putObject(PutObjectArgs.builder().bucket(bucket).stream(go,go.available(),-1).object("go.jpg").contentType("image/jpeg").build());
-//
-//    }
-//    @Test
-//    void getImg() throws IOException, InvalidKeyException, InvalidResponseException, InsufficientDataException, NoSuchAlgorithmException, ServerException, InternalException, XmlParserException, ErrorResponseException {
-//        String bk = "2021-4";
-//        String name = "go.jpg";
-//        InputStream object = null;
-//        try {
-//            object = client.getObject(bk,name);
-//        }
-//        System.out.println(object);
-//    }
-//
-//}
+package pers.mtx.resources;
+
+import io.minio.*;
+import io.minio.errors.*;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Date;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ResourcesApplication.class)
+class ResourcesApplicationTests {
+
+    @Autowired
+    MinioClient client;
+
+    @Test
+    void contextLoads() throws IOException, InvalidKeyException, InvalidResponseException, InsufficientDataException, NoSuchAlgorithmException, ServerException, InternalException, XmlParserException, ErrorResponseException {
+        //使用默认时区和语言环境获得一个日历。
+        Calendar rightNow    =    Calendar.getInstance();
+        /*用Calendar的get(int field)方法返回给定日历字段的值。
+        HOUR 用于 12 小时制时钟 (0 - 11)，HOUR_OF_DAY 用于 24 小时制时钟。*/
+        Integer year = rightNow.get(Calendar.YEAR);
+        Integer month = rightNow.get(Calendar.MONTH)+1; //第一个月从0开始，所以得到月份＋1
+        Integer day = rightNow.get(Calendar.DAY_OF_MONTH);
+        String bucket = year+"-"+month;
+        boolean exists = client.bucketExists(BucketExistsArgs.builder().bucket(bucket).build());
+        if (!exists){
+            client.makeBucket(MakeBucketArgs.builder().bucket(bucket).build());
+
+        }
+        File img = new File("D:\\demo.png");
+        InputStream go = new FileInputStream(img);
+        InputStream stream = new ByteArrayInputStream("wdawdawdawdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasssssssssssssssssssssssssssssddddddddddddddddddddddddddddddd".getBytes(StandardCharsets.UTF_8));
+        client.putObject(PutObjectArgs.builder().bucket(bucket).stream(go,go.available(),-1).object("go.png").contentType("image/png").build());
+
+    }
+    @Test
+    void getImg() throws IOException, InvalidKeyException, InvalidResponseException, InsufficientDataException, NoSuchAlgorithmException, ServerException, InternalException, XmlParserException, ErrorResponseException {
+        String bk = "2021-4";
+        String name = "go.jpg";
+        InputStream object = null;
+
+        System.out.println(object);
+    }
+
+}
